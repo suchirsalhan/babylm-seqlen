4c5916ceaf6e4ee9280a34c904085bb16dc0cb3e
Suchir Salhan <suchirsalhan@gmail.com>
2025-04-22 10:07:06 +0100

    Update collator.py

--- src/collator.py
+++ src/collator.py
@@ -1,10 +1,14 @@
 import torch
-import json
-import os
 from torch.nn import functional as F
-from transformers import Trainer, DataCollatorForLanguageModeling
+from transformers import DataCollatorForLanguageModeling
+
 class CustomDataCollator(DataCollatorForLanguageModeling):
+    def __init__(self, mlm=False):
+        super().__init__(mlm=mlm)
+
     def __call__(self, features):
+        # We don't need to handle 'attention_mask' for pre-tokenized datasets
         for f in features:
-            f.pop("attention_mask", None)
+            f.pop("attention_mask", None)  # Remove 'attention_mask' if present
+        
         return super().__call__(features)
